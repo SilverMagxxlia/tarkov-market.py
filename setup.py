dd6f525123b92bda9b49d842a2855c9b155b3c70
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-08-25 19:37:59 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import re
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+version = ''
+with open('discord/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+readme = ''
+
+with open('README.rst') as f:
+    readme = f.read()
+
+packages = [
+    'tarkov_market',
+    'tarkov_market.types'
+]
+
+setup(
+    name='tarkov-market.py',
+    url='https://github.com/Hostagen/tarkov-market.py',
+    description='async API wrapper for Tarkov Market written in Python.',
+    long_description=readme,
+    version=version,
+    packages=packages,
+    author='Hostagen',
+    license='MIT',
+    python_requires='>=3.8.0',
+)
