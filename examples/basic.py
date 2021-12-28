d262cafeee27e550b9dba7e5dc98066cbf296396
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-12-28 23:51:43 +0900

    update example.

--- examples/basic.py
+++ examples/basic.py
@@ -3,19 +3,21 @@ import tarkov_market
 
 from typing import List
 
-market = tarkov_market.Client(token='INSERT YOUR API KEY.')
+loop = asyncio.get_event_loop()
+market = tarkov_market.Client(token='INSERT YOUR API KEY.', refresh_rate=None)
 
 
 async def main():
-    # Must run setup once before use.
-    await market.setup()
-
     # Find Item and return Items from pre-loaded Item List. It's Unlimited.
-    item: List[tarkov_market.Item] = market.find_items('TerraGroup Labs keycard (Red)')
+    items: List[tarkov_market.Item] = market.find_items('keycard')
 
-    return item
+    # Get Item by name. Can get it through uid and bsg_id.
+    item: tarkov_market.Item
+    item = market.get_item('Tactical glasses')
+    item = market.get_item(bsg_id='BSG_ID')
+    item = market.get_item(uid='UID')
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
+    market.start()
     loop.run_until_complete(main())
-    loop.close()
+    loop.run_forever()
