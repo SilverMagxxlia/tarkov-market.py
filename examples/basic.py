b4c9b0a737c6365082f18cb7f0812f29dc2e3567
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-12-31 13:44:14 +0900

    edit example text.

--- examples/basic.py
+++ examples/basic.py
@@ -8,7 +8,7 @@ market = tarkov_market.Client(token='INSERT YOUR API KEY.', refresh_rate=None)
 
 
 async def main():
-    # Find Item and return Items from pre-loaded Item List. It's Unlimited.
+    # Find Item and return Items from preloaded Item List. It's Unlimited.
     items: List[tarkov_market.Item] = market.find_items('keycard')
 
     # Get Item by name. Can get it through uid and bsg_id.
