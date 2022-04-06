293e9f741c710b35545a3d76d6608dca4652ef77
Hostagen <68284806+Hostagen@users.noreply.github.com>
2022-04-06 23:47:37 +0900

    replace _RawTrader to Trader

--- tarkov_market/types/trader.py
+++ tarkov_market/types/trader.py
@@ -3,13 +3,8 @@ from typing import TypedDict, Literal
 currencies = Literal['₽', '$', '€']
 
 
-class _RawTrader(TypedDict, total=False):
+class Trader(TypedDict, total=False):
     traderName: str
     traderPrice: int
     traderPriceCur: currencies
-
-
-class Trader(TypedDict, total=False):
-    name: str
-    price: int
-    currency: currencies
+    traderPriceRub: int
