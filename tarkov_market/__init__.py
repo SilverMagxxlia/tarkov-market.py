45eb2d6e1e513ec8c0ab185ec60283bbf8072720
Hostagen <68284806+Hostagen@users.noreply.github.com>
2022-04-06 23:54:10 +0900

    upgrade version.

--- tarkov_market/__init__.py
+++ tarkov_market/__init__.py
@@ -10,7 +10,7 @@ Tarkov Market API Wrapper
 __title__ = 'tarkov_market'
 __author__ = 'Hostagen'
 __license__ = 'MIT'
-__version__ = '0.11.0'
+__version__ = '0.12.0'
 
 from .client import *
 from .errors import *
