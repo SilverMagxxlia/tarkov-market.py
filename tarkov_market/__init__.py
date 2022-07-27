c5be7ce165e365903edc8566b4041c40bc4c7fde
hostagen <skillpoint0225@gmail.com>
2022-07-27 15:26:01 +0900

    upgrade version

--- tarkov_market/__init__.py
+++ tarkov_market/__init__.py
@@ -1,16 +1,15 @@
 """
-Tarkov Market API Wrapper
-~~~~~~~~~~~~~~~~
 
+Tarkov Market API Wrapper
+~~~~~~~~~~~~~~~~~~~~~~~~~~
 :copyright: (c) 2021-present Hostagen
 :license: MIT, see LICENSE for more details.
-
 """
 
 __title__ = 'tarkov_market'
 __author__ = 'Hostagen'
 __license__ = 'MIT'
-__version__ = '0.13.1'
+__version__ = '0.14.0'
 
 from .client import *
 from .errors import *
