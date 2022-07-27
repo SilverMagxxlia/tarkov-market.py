7848bdcc88c45cd684d5d8be1482c2dce5b56952
hostagen <skillpoint0225@gmail.com>
2022-07-27 15:17:43 +0900

    edit example fetch

--- examples/fetch.py
+++ examples/fetch.py
@@ -2,23 +2,35 @@ import asyncio
 import tarkov_market
 
 from typing import List
+from tarkov_market import Item
 
-loop = asyncio.get_event_loop()
-market = tarkov_market.Client(token='INSERT YOUR API KEY.', loop=loop)
+TOKEN: str = 'YOUR API KEY'
+market = tarkov_market.Client(token=TOKEN, refresh_rate=None)
 
 
-async def main():
-    # fetch the latest data from tarkov-market.
-    # there is a limit of 300 requests per minute to fetch.
+async def main() -> None:
+    # return only one of the search results
+    item: Item = await market.fetch_item('TerraGroup Labs keycard (Red)')
 
-    # return the first data item from the request result.
-    item: tarkov_market.Item = await market.fetch_item('TerraGroup Labs keycard (Red)')
+    print(item.name, item.price)
 
-    # return the items from the request results.
-    items: List[tarkov_market.Item] = await market.fetch_items('keycard')
+    # return all search results
+    items: List[Item] = await market.fetch_items('key')
 
-    return item
+    for item in items:
+        print(item.name, item.price)
+
+    '''
+    The fetch functions has a limit of 300 requests per minute because it communicates directly with the API.
+    If you have a large number of requests, check the example in the link below.
+    
+    cache example: https://github.com/Hostagen/tarkov-market.py/blob/master/examples/cache.py
+    
+    Unrestricted by pre-loading and caching data once.
+    '''
+
 
 if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-    loop.close()
+    loop.run_forever()
